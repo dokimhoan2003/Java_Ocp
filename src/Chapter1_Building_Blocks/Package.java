cea6951d52a29075eb61b458faee2fb59c1f95cc
Do Kim Hoan <kimhoando2003@gmail.com>
2025-06-30 01:02:35 +0700

    complie, run, achive java files

---
+++ src/Chapter1_Building_Blocks/Package.java
@@ -0,0 +1,13 @@
+package Chapter1_Building_Blocks;
+
+import java.util.Random;
+
+public class Package {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+    System.out.println(random.nextInt(100));
+
+  }
+
+}
