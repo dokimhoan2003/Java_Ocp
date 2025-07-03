9b1b689eb1b6ba3d941bc67f09cc6158b3e59461
Do Kim Hoan <kimhoando2003@gmail.com>
2025-07-03 07:24:38 +0700

    text bloclks lesson

---
+++ src/Chapter1_Building_Blocks/TextBlocks.java
@@ -0,0 +1,12 @@
+package Chapter1_Building_Blocks;
+
+public class TextBlocks {
+
+  public static void main(String[] args) {
+    String expamle = """
+      "This is a text block."
+        It can span multiple lines
+      """;
+    System.out.println(expamle + "Hoan");
+  }
+}
